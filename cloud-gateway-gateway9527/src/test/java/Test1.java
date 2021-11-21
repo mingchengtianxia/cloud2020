e4dfac77e9f42966dfba26743a140eae26f11bf4
mingchengtianxia <xuminggang_1@126.com>
2021-11-21 10:05:29 +0800

    添加gateway网关服务，并实现动态路由

---
+++ cloud-gateway-gateway9527/src/test/java/Test1.java
@@ -0,0 +1,13 @@
+import java.time.ZonedDateTime;
+
+/**
+ * @author mingchengtianxia
+ * @date 2021/11/14--17:38
+ */
+public class Test1 {
+
+	public static void main(String[] args) {
+		ZonedDateTime time = ZonedDateTime.now();
+		System.out.println(time);
+	}
+}
